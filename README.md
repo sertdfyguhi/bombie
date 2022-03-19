5e91248bd339db396a20ab8bdfa20fd7b9788280
sertdfyguhi-git <gdkamtalan@gmail.com>
2022-03-20 01:16:49 +0800

    elixir

--- README.md
+++ README.md
@@ -21,3 +21,4 @@ print(bombie('bombie'))
 - python
 - spwn
 - shitlang
+- elixir
---
+++ langs/bombie.ex
@@ -0,0 +1,7 @@
+defmodule Bombie do
+  def bombie(str) do
+    IO.puts str <> "."
+  end
+end
+
+Bombie.bombie("bombie")
